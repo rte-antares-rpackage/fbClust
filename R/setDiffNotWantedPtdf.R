ba544bb846506b1ba3ec45999627dd49dbf66088
JulienDstorm <julien.bretteville@datastorm.fr>
2019-03-20 10:12:12 +0100

    fichiers tests + modifs test diffnotwanted

--- R/setDiffNotWantedPtdf.R
+++ R/setDiffNotWantedPtdf.R
@@ -7,18 +7,21 @@
 #' 
 setDiffNotWantedPtdf <- function(PLAN, not_wanted_col = NULL)
 {
-  PLAN2 <- copy(PLAN)
-  col_ptdf <- colnames(PLAN2)[grep("ptdf", colnames(PLAN2))]
+  # PLAN2 <- copy(PLAN)
+  col_ptdf <- colnames(PLAN)[grep("ptdf", colnames(PLAN))]
   if (is.null(not_wanted_col)) {
     not_wanted_col <-  col_ptdf[length(col_ptdf)]
   }
   col_ptdf <- col_ptdf[-which(col_ptdf == not_wanted_col)]
 
-  PLAN2[, c(col_ptdf) := lapply(col_ptdf, function(ptdf) {
-    PLAN2[[ptdf]] - PLAN2[[not_wanted_col]]
-  })]
-  PLAN2[[not_wanted_col]] <- NULL
-  return(PLAN2)
+  PLAN[, col_ptdf] <- sapply(col_ptdf, function(ptdf) {
+    PLAN[[ptdf]] <- PLAN[[ptdf]] - PLAN[[not_wanted_col]]
+  })
+  # PLAN2[, c(col_ptdf) := lapply(col_ptdf, function(ptdf) {
+  #   PLAN2[[ptdf]] - PLAN2[[not_wanted_col]]
+  # })]
+  PLAN[[not_wanted_col]] <- NULL
+  return(PLAN)
 }
 
 
--- tests/testthat/test-setDiffNotWantedtdf.R
+++ tests/testthat/test-setDiffNotWantedtdf.R
@@ -1,13 +1,13 @@
-# context("Function setDiffNotWantedPtdf")
-# 
-# test_that("setDiffNotWantedPtdf", {
-#   library(data.table)
-#   PLAN <- data.table("ptdfXX" = seq(900,1000, 4), "ptdfYY" = seq(600,650, 2),
-#                      "ptdfZZ" = seq(300, 400, 4), "bla" = seq(500, 525, 1))
-#   plan_res <- setDiffNotWantedPtdf(PLAN = PLAN, not_wanted_col = "ptdfYY")
-# 
-#   expect_true(all(plan_res[, bla] == PLAN[, bla]))
-#   expect_true(all(plan_res[, ptdfXX] == PLAN[, ptdfXX - ptdfYY]))
-#   expect_true(all(plan_res[, ptdfZZ] == PLAN[, ptdfZZ - ptdfYY]))
-#   expect_true(ncol(plan_res) == (ncol(PLAN) - 1))
-# })
+context("Function setDiffNotWantedPtdf")
+
+test_that("setDiffNotWantedPtdf", {
+  library(data.table)
+  PLAN <- data.table("ptdfXX" = seq(900,1000, 4), "ptdfYY" = seq(600,650, 2),
+                     "ptdfZZ" = seq(300, 400, 4), "bla" = seq(500, 525, 1))
+  plan_res <- setDiffNotWantedPtdf(PLAN = PLAN, not_wanted_col = "ptdfYY")
+
+  expect_true(all(plan_res[['bla']] == PLAN[['bla']]))
+  expect_true(all(plan_res[['ptdfXX']] == PLAN[['ptdfXX']] - PLAN[['ptdfYY']]))
+  expect_true(all(plan_res[['ptdfZZ']] == PLAN[['ptdfZZ']] - PLAN[['ptdfYY']]))
+  expect_true(ncol(plan_res) == (ncol(PLAN) - 1))
+})
