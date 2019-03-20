67edfac89f134526fba470661050e9d3f9e81c16
JulienDstorm <julien.bretteville@datastorm.fr>
2019-03-20 14:09:41 +0100

    test transformTs

---
+++ tests/testthat/test-transformTs.R
@@ -0,0 +1,19 @@
+context("Function .transformTS")
+
+test_that(".transformTS", {
+  library(data.table)
+  PLAN <- fread(system.file("testdata/190318_plan.csv", package = "fbClust"), sep = "|")
+
+  
+  PLAN_date <- .transformTS(PLAN)
+  expect_true(is.null(PLAN_date$timestamp))
+  expect_true(all(grepl(pattern = "^[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]$", PLAN_date$Date)))
+  expect_true(all(PLAN_date$Period >= 1 & PLAN_date$Period <= 12))
+  expect_true(all(colnames(PLAN_date) == c(
+    colnames(PLAN)[-grep("timestamp", colnames(PLAN))], "Period", "Date")))
+  
+})
+
+
+
+
