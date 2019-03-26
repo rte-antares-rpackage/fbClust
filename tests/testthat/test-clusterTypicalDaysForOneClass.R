fcea32533446db27b4a9bb8a99d658514a82e727
JulienDstorm <julien.bretteville@datastorm.fr>
2019-03-26 11:16:43 +0100

    test grosse fonction avec VERT déjà créé

--- tests/testthat/test-clusterTypicalDaysForOneClass.R
+++ tests/testthat/test-clusterTypicalDaysForOneClass.R
@@ -5,18 +5,20 @@ test_that("clusterTypicalDaysForOneClass", {
   library(quadprog)
   library(fbTools)
 
-  PLAN <- readRDS(system.file("testdata/plan_not_wanted_ptdf.rds", package = "fbClust"))
+  PL_VE <- readRDS(system.file("testdata/plan_not_wanted_ptdf.rds", package = "fbClust"))
+  PLAN <- PL_VE[[1]]
+  VERT <- PL_VE[[2]]
   dates <- seq(as.Date("2019-02-14"), as.Date("2019-02-17"), by = "day")
   nbcluster <- 2
   maxDomainSize <- 20000
   allTypDays <- clusterTypicalDaysForOneClass(
-    dates = dates, PLAN = PLAN, VERT = NULL,
+    dates = dates, PLAN = PLAN, VERT = VERT,
     maxDomainSize = maxDomainSize, nbCluster = nbcluster,
     report = F)
 
   # maxDomainSize2 <- 2000
   # 
-  # expect_true(nrow(allTypDays) == nbcluster)
+  expect_true(nrow(allTypDays) == nbcluster)
   # 
   # expect_error(clusterTypicalDaysForOneClass(
   #   dates = dates, PLAN = PLAN, VERT = NULL,
