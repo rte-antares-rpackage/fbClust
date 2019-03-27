3e6e206777e03c63afbdd59335295ab761937a80
JulienDstorm <julien.bretteville@datastorm.fr>
2019-03-27 12:16:14 +0100

    mroe tests on big function

--- tests/testthat/test-clusterTypicalDaysForOneClass.R
+++ tests/testthat/test-clusterTypicalDaysForOneClass.R
@@ -3,7 +3,6 @@ context("Function clusterTypicalDaysForOneClass")
 test_that("clusterTypicalDaysForOneClass", {
   library(data.table)
   library(quadprog)
-  library(fbTools)
 
   PL_VE <- readRDS(system.file("testdata/plan_not_wanted_ptdf.rds", package = "fbClust"))
   PLAN <- PL_VE[[1]]
@@ -17,13 +16,13 @@ test_that("clusterTypicalDaysForOneClass", {
     report = F, hourWeight = rep(1, 24), className = NULL,
     reportPath = NULL, id_start = 1)
 
-  # maxDomainSize2 <- 2000
-  #
+  maxDomainSize2 <- 2000
+
   expect_true(nrow(allTypDays) == nbcluster)
-  #
-  # expect_error(clusterTypicalDaysForOneClass(
-  #   dates = dates, PLAN = PLAN, VERT = NULL,
-  #   maxDomainSize = maxDomainSize2, nbCluster = nbcluster,
-  #   report = F))
+
+  expect_error(clusterTypicalDaysForOneClass(
+    dates = dates, PLAN = PLAN, VERT = NULL,
+    maxDomainSize = maxDomainSize2, nbCluster = nbcluster,
+    report = F))
 
 })
