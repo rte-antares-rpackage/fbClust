13d379f42507932e2b723b9b99b8267b354e7588
JulienDstorm <julien.bretteville@datastorm.fr>
2019-03-27 10:13:12 +0100

    nouveau test pour get distmatrix

--- R/distMatrix.R
+++ R/distMatrix.R
@@ -10,14 +10,17 @@
   # PLAN[, ram := ram + runif(nrow(PLAN))/10000]
   col_ptdf <- colnames(PLAN)[grep("ptdf", colnames(PLAN))]
   print(col_ptdf)
-  # res_hour <- data.table(t(combn(unique(VERT[, Date]), 2)))
+  # res_hour <- data.table(t(combn(unique(VERT[, .SD, .SDcols = "Date"]), 2)))
   print(unique(VERT[['Date']]))
   res_hour <- data.frame(t(combn(unique(VERT[['Date']]), 2)))
+  # setDT(res_hour)
   colnames(res_hour) <- c("V1", "V2")
   print(res_hour)
   data <- rbindlist(sapply(1:nrow(res_hour), function(comb){
     
     ##To sapply
+    # date_1 <- as.character(res_hour[comb, .SD, .SDcols = "V1"])
+    # date_2 <- as.character(res_hour[comb, .SD, .SDcols = "V2"])
     date_1 <- as.character(res_hour[comb, "V1"])
     date_2 <- as.character(res_hour[comb, "V2"])
     print(date_1)
@@ -53,6 +56,9 @@
       data <- data.table(Date1 = c(date_1, date_2),
                  Date2 = c(date_2, date_1), Period = h, dist = d)
       print("hello")
+      print(data)
+      print(class(data))
+      setDT(data)
       data
     }, simplify = FALSE))
     
--- tests/testthat/test-getDistMatrixV2.R
+++ tests/testthat/test-getDistMatrixV2.R
@@ -4,12 +4,14 @@ test_that(".getDistMatrixV2", {
   library(data.table)
   library(quadprog)
 
-  pl_vert <- readRDS(system.file("testdata/pl_vert_test.rds", package = "fbClust"))
-  # PL_VE <- readRDS(system.file("testdata/plan_not_wanted_ptdf.rds", package = "fbClust"))
-  VERT <- pl_vert[[2]]
-  PLAN <- pl_vert[[1]]
-  # VERT <- PL_VE[[2]]
-  # PLAN <- PL_VE[[1]]
+  # pl_vert <- readRDS(system.file("testdata/pl_vert_test.rds", package = "fbClust"))
+  PL_VE <- readRDS(system.file("testdata/plan_not_wanted_ptdf.rds", package = "fbClust"))
+  # VERT <- pl_vert[[2]]
+  # PLAN <- pl_vert[[1]]
+  VERT <- PL_VE[[2]]
+  PLAN <- PL_VE[[1]]
+  PLAN <- .transformTS(PLAN)
+  VERT <- .transformTS(VERT)
   VERT <- setDT(VERT)
   PLAN <- setDT(PLAN)
   
@@ -17,6 +19,6 @@ test_that(".getDistMatrixV2", {
   res <- .getDistMatrixV2(VERT = VERT, PLAN = PLAN, hourWeight = hourWeight)
   expect_true(is.data.table(res))
   expect_true(length(unique(res[, "dist"])) == 1)
-  expect_true(res[1, "dist"] < 780 & res[1, "dist"] > 778)
-  expect_true(nrow(res) == 2 & ncol(res) == 4)
+  expect_true(res[1, "dist"] < 782 & res[1, "dist"] > 778)
+  expect_true(nrow(res) == 12 & ncol(res) == 4)
 })
